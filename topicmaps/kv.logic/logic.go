016b0f9a63efd259e0431590290dca891eeed518
Joshua Tacoma <joshua.tacoma@gmail.com>
2019-07-25 22:28:33 -0400

    Add a couple of plausible functions to kv.logic

--- topicmaps/kv.logic/logic.go
+++ topicmaps/kv.logic/logic.go
@@ -15,10 +15,11 @@
 package logic
 
 import (
+	"github.com/google/note-maps/kv"
 	"github.com/google/note-maps/topicmaps/kv.models"
 )
 
-// Store adds business logic to models.Store.
+// Store adds some application logic to models.Store.
 type Store struct{ models.Store }
 
 // CreateTopicMap creates a new topic map in s and returns a copy of the topic
@@ -31,4 +32,38 @@ func (s *Store) CreateTopicMap() (*models.TopicMapInfo, error) {
 	info := &models.TopicMapInfo{}
 	info.TopicMap = uint64(entity)
 	return info, s.SetTopicMapInfo(entity, info)
+}
+
+func (s *Store) CreateTopicWithName(name string) (kv.Entity, error) {
+	t, err := s.Alloc()
+	if err != nil {
+		return 0, err
+	}
+	n, err := s.Alloc()
+	if err != nil {
+		return 0, err
+	}
+	var m models.Name
+	m.Topic = uint64(t)
+	m.Value = name
+	if err := s.SetName(n, &m); err != nil {
+		return 0, err
+	}
+	return t, s.SetTopicNames(t, models.TopicNames{n})
+}
+
+func (s *Store) TopicsByName(c *kv.IndexCursor, n int) ([]kv.Entity, error) {
+	ns, err := s.EntitiesByNameValue(c, n)
+	if err != nil {
+		return nil, err
+	}
+	names, err := s.GetNameSlice(ns)
+	if err != nil {
+		return nil, err
+	}
+	ts := make([]kv.Entity, len(names))
+	for i := range names {
+		ts[i] = kv.Entity(names[i].Topic)
+	}
+	return ts, nil
 }

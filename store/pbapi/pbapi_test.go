e648a37f7b7ad763c9d9fae81f1c58d32accc3b6
Joshua Tacoma <joshua.tacoma@gmail.com>
2019-08-16 19:14:34 -0400

    Add tests for the pbapi module

---
+++ store/pbapi/pbapi_test.go
@@ -0,0 +1,51 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pbapi
+
+import (
+	"testing"
+
+	"github.com/google/note-maps/kv/kvtest"
+	"github.com/google/note-maps/store/pb"
+)
+
+func TestCreateGetTopicMap(t *testing.T) {
+	db := kvtest.NewDB(t)
+	defer db.Close()
+	g := NewGateway(db)
+	createResponse, err := g.CreateTopicMap(&pb.CreateTopicMapRequest{})
+	if err != nil {
+		t.Fatal(err)
+	} else if createResponse.TopicMap == nil {
+		t.Fatal("want non-nil TopicMap, got nil")
+	} else if createResponse.TopicMap.Id == 0 {
+		t.Error("want non-zero TopicMap.Id, got zero")
+	}
+	if createResponse.TopicMap.Topic == nil {
+		t.Fatal("want non-nil TopicMap.Topic, got nil")
+	} else if createResponse.TopicMap.Topic.Id == 0 {
+		t.Fatal("want non-zero TopicMap.Topic.Id, got nil")
+	} else if createResponse.TopicMap.Topic.Id != createResponse.TopicMap.Id {
+		t.Fatalf("want TopicMap.Id==TopicMap.Topic.Id, got %v!=%v",
+			createResponse.TopicMap.Id,
+			createResponse.TopicMap.Topic.Id)
+	}
+	getResponse, err := g.GetTopicMaps(&pb.GetTopicMapsRequest{})
+	if err != nil {
+		t.Fatal(err)
+	} else if len(getResponse.TopicMaps) == 0 {
+		t.Error("want non-empty list of topic maps, got empty")
+	}
+}

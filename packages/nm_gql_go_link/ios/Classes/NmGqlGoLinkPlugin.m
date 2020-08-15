a24e59d2a92923e422095650f06b04534a687eb5
Joshua Tacoma <jtacoma@google.com>
2020-08-15 11:07:24 -0400

    packages/nm_gql_go_link: implement for ios

--- packages/nm_gql_go_link/ios/Classes/NmGqlGoLinkPlugin.m
+++ packages/nm_gql_go_link/ios/Classes/NmGqlGoLinkPlugin.m
@@ -1,15 +1,50 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #import "NmGqlGoLinkPlugin.h"
-#if __has_include(<nm_gql_go_link/nm_gql_go_link-Swift.h>)
-#import <nm_gql_go_link/nm_gql_go_link-Swift.h>
-#else
-// Support project import fallback if the generated compatibility header
-// is not copied when this plugin is created as a library.
-// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "nm_gql_go_link-Swift.h"
-#endif
+#import "../Frameworks/GoMobileNmgql.framework/Headers/GoMobileNmgql.h"
 
 @implementation NmGqlGoLinkPlugin
+
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftNmGqlGoLinkPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+            methodChannelWithName:@"nm_gql_go_link"
+                  binaryMessenger:[registrar messenger]];
+    NmGqlGoLinkPlugin* instance = [[NmGqlGoLinkPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([@"getPlatformVersion" isEqualToString:call.method]) {
+        result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+    } else if ([@"getGoVersion" isEqualToString:call.method]) {
+        NSString* response = GoMobileNmgqlGetGoVersion();
+        result(response);
+    } else if ([@"gqlRequest" isEqualToString:call.method]) {
+        FlutterStandardTypedData* request = call.arguments[@"request"];
+        NSError* error = NULL;
+        NSData* response = GoMobileNmgqlRequest(request.data, &error);
+        if (error != NULL) {
+            result([FlutterError errorWithCode:@"UNAVAILABLE"
+                                 message:[error localizedDescription]
+                                 details:nil]);
+        } else {
+            result(response);
+        }
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
 @end

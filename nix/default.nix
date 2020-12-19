5b425c8ef56e8094302eb56637aa5e351932e376
Joshua Tacoma <jtacoma@google.com>
2020-12-19 16:08:15 -0500

    build: nix: use androidenv for SDK

--- nix/default.nix
+++ nix/default.nix
@@ -27,6 +27,7 @@ let
         ; flutter-dev = flutterPackages.dev; } ) ];
   };
   pkgs = import sources.nixpkgs {
+    config.android_sdk.accept_license = true;
     overlays = [
       (_: pkgs:
 	{ inherit (unstable) flutter-dev;
@@ -37,6 +38,26 @@ let
   src = gitignoreSource ./..;
   lib = pkgs.lib;
   stdenv = pkgs.stdenv;
+  androidComposition = pkgs.androidenv.composeAndroidPackages {
+    ##toolsVersion = "25.2.5";
+    ##platformToolsVersion = "27.0.1";
+    ##buildToolsVersions = [ "27.0.3" ];
+    #includeEmulator = false;
+    ##emulatorVersion = "27.2.0";
+    ##platformVersions = [ "24" ];
+    #includeSources = false;
+    #includeDocs = false;
+    #includeSystemImages = false;
+    ##systemImageTypes = [ "default" ];
+    ##abiVersions = [ "armeabi-v7a" ];
+    ##lldbVersions = [ "2.0.2558144" ];
+    ##cmakeVersions = [ "3.6.4111459" ];
+    #includeNDK = true;
+    ##ndkVersion = "16.1.4479499";
+    #useGoogleAPIs = false;
+    #useGoogleTVAddOns = false;
+    ##includeExtras = [ "extras;google;gcm" ];
+  };
 in rec
 {
   inherit pkgs src;
@@ -52,6 +73,7 @@ in rec
     inherit (pkgs) git; # required by flutter!
     inherit (pkgs) gnumake;
     inherit (pkgs) go;
+    inherit (androidComposition) androidsdk;
   };
 
   # Additional tools required to build Note Maps in a more controlled

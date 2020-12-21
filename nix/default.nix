0cd694ccf7427da7c3dd5a0a31ce23397a92b45a
Joshua Tacoma <jtacoma@google.com>
2020-12-21 09:46:59 -0600

    build: nix: specify android composition

--- nix/default.nix
+++ nix/default.nix
@@ -43,26 +43,14 @@ let
   src = gitignoreSource ./..;
   lib = pkgs.lib;
   stdenv = pkgs.stdenv;
-  androidComposition = pkgs.androidenv.composeAndroidPackages {
-    ##toolsVersion = "25.2.5";
-    ##platformToolsVersion = "27.0.1";
-    ##buildToolsVersions = [ "27.0.3" ];
-    #includeEmulator = false;
-    ##emulatorVersion = "27.2.0";
-    ##platformVersions = [ "24" ];
-    #includeSources = false;
-    #includeDocs = false;
-    #includeSystemImages = false;
-    ##systemImageTypes = [ "default" ];
-    ##abiVersions = [ "armeabi-v7a" ];
-    ##lldbVersions = [ "2.0.2558144" ];
-    ##cmakeVersions = [ "3.6.4111459" ];
-    #includeNDK = true;
-    ##ndkVersion = "16.1.4479499";
-    #useGoogleAPIs = false;
-    #useGoogleTVAddOns = false;
-    ##includeExtras = [ "extras;google;gcm" ];
-  };
+  androidsdk = (pkgs.androidenv.composeAndroidPackages {
+    buildToolsVersions = [ "28.0.3" ];
+    platformVersions = [ "29" ];
+    platformToolsVersion = "29.0.6";
+    includeNDK = true;
+    ndkVersion = "21.0.6113669";
+    abiVersions = [ "x86-64" ];
+  }).androidsdk;
 in rec
 {
   inherit pkgs src;

5cb81dd7e58ef55e5ca8d2ee5cfc8c0093c299b3
Joshua Tacoma <jtacoma@google.com>
2020-12-26 17:00:11 -0600

    third_party/nixpkgs/flutter: update all channels
    Tested:
      for channel in stable beta dev ; do
         nix-build ./nix -A pkgs.flutterPackages.$channel && flutter doctor
      done

--- third_party/nixpkgs/flutter/default.nix
+++ third_party/nixpkgs/flutter/default.nix
@@ -1,9 +1,9 @@
 { callPackage, dart }:
 
 let
-  dart_stable = dart.override { version = "2.10.0"; };
-  dart_beta = dart.override { version = "2.10.0"; };
-  dart_dev = dart.override { version = "2.11.0-161.0.dev"; };
+  dart_stable = dart.override { version = "2.10.4"; };
+  dart_beta = dart.override { version = "2.12.0-133.2.beta"; };
+  dart_dev = dart.override { version = "2.12.0-141.0.dev"; };
   mkFlutter = opts: callPackage (import ./flutter.nix opts) { };
   getPatches = dir:
     let files = builtins.attrNames (builtins.readDir dir);
@@ -13,28 +13,31 @@ in {
   stable = mkFlutter rec {
     pname = "flutter";
     channel = "stable";
-    version = "1.22.0";
+    version = "1.22.5";
     filename = "flutter_linux_${version}-${channel}.tar.xz";
-    sha256Hash = "0ryrx458ss8ryhmspcfrhjvad2pl46bxh1qk5vzwzhxiqdc79vm8";
+    sha256Hash = "1dv5kczcj9npf7xxlanmpc9ijnxa3ap46521cxn14c0i3y9295ja";
     patches = getPatches ./patches/stable;
     dart = dart_stable;
+    dartPackages = import ./stable.nix;
   };
   beta = mkFlutter rec {
     pname = "flutter";
     channel = "beta";
-    version = "1.22.0-12.3.pre";
+    version = "1.25.0-8.1.pre";
     filename = "flutter_linux_${version}-${channel}.tar.xz";
-    sha256Hash = "1axzz137z4lgpa09h7bjf52i6dij6a9wmjbha1182db23r09plzh";
-    patches = getPatches ./patches/stable;
+    sha256Hash = "01rfs7n3ghaffqrjq44libgb1iyrr1h2br995inf1l6vqi78mcld";
+    patches = getPatches ./patches/dev;
     dart = dart_beta;
+    dartPackages = import ./beta.nix;
   };
   dev = mkFlutter rec {
     pname = "flutter";
     channel = "dev";
-    version = "1.23.0-7.0.pre";
+    version = "1.26.0-1.0.pre";
     filename = "flutter_linux_${version}-${channel}.tar.xz";
-    sha256Hash = "166qb4qbv051bc71yj7c0vrkamhvzz3fp3mz318qzm947mydwjj5";
+    sha256Hash = "1nqyx6k1a2yynl9j5ffx1fkv2r763xnqcpmgj47h58sn318hbmaw";
     patches = getPatches ./patches/dev;
     dart = dart_dev;
+    dartPackages = import ./dev.nix;
   };
 }
---
+++ third_party/nixpkgs/flutter/dev.nix
@@ -0,0 +1,442 @@
+[
+  {
+    name = "_fe_analyzer_shared";
+    version = "12.0.0";
+    sha256 = "0sr0afpdgddf06zwvrx7lcy6dz05ha797k7s0z216kap9hlr4j82";
+  }
+  {
+    name = "analyzer";
+    version = "0.40.6";
+    sha256 = "0nqh4sqchl1nd1hkl3xavsn8kwvcblj12hx8h51wip043c7nisri";
+  }
+  {
+    name = "archive";
+    version = "2.0.13";
+    sha256 = "1bjhryz29z0qkaz1sz32g44nskfs5jsx7i99m42ng25hszpk4qzh";
+  }
+  {
+    name = "args";
+    version = "1.6.0";
+    sha256 = "0j0kxwilslxpixqfyvazv2f6kp52sv7jlrb488argg1h8218b9vb";
+  }
+  {
+    name = "async";
+    version = "2.5.0-nullsafety.3";
+    sha256 = "0h2vyxzsz2wss0hdjd69cxqz7d4hgvzddifjw70qh8nric6fqg88";
+  }
+  {
+    name = "boolean_selector";
+    version = "2.1.0-nullsafety.3";
+    sha256 = "02281slh678vaa15yqzvd0vlwngaab5aq6bj4dd914k0wffdmpw6";
+  }
+  {
+    name = "browser_launcher";
+    version = "0.1.8";
+    sha256 = "1nk9i27wwzdyrgwc48msafi7n4jj8mzsvhj7x844fym9zlkqvf7l";
+  }
+  {
+    name = "built_collection";
+    version = "4.3.2";
+    sha256 = "1gljif531n5mm0hynqd0l9g1xb87jxygf40j0ci28n8h6dr1nx2b";
+  }
+  {
+    name = "built_value";
+    version = "7.1.0";
+    sha256 = "1yaxpgyl85ijvn5h2v4hj61327pa0pf0dzbrqcjmfd25ig1bwz4c";
+  }
+  {
+    name = "charcode";
+    version = "1.2.0-nullsafety.3";
+    sha256 = "00zlmxgsza9v3g0g091ygh8mr9cfwry2bwgv09myishrx3bd9323";
+  }
+  {
+    name = "checked_yaml";
+    version = "1.0.2";
+    sha256 = "0hrbvshp76xv259n7ns7bpgiirff8qrpdnn6kj34ldrkfn9j15zl";
+  }
+  {
+    name = "cli_util";
+    version = "0.2.0";
+    sha256 = "09xsy6g5rc8bhjw8z7s4nz7c9dqal9162k0imcmvfaygfvy7i6qn";
+  }
+  {
+    name = "clock";
+    version = "1.1.0-nullsafety.3";
+    sha256 = "0r14yac286pks91xlg051063czlimxk93473k172c8fd3y4m2za7";
+  }
+  {
+    name = "collection";
+    version = "1.15.0-nullsafety.5";
+    sha256 = "1qq01nnp0yqr4x72xckybxay460m2glj3pp4j9m91sfhyg7mmjhl";
+  }
+  {
+    name = "completion";
+    version = "0.2.3";
+    sha256 = "15f2dsp645bfs5xam615q11w13shb4mrnl2avxcf4plkhhj2lpra";
+  }
+  {
+    name = "convert";
+    version = "2.1.1";
+    sha256 = "1zff9lifka6y4zm4ybsvyp6y6j3xfh1schs4h5aisvz22z23pa1z";
+  }
+  {
+    name = "coverage";
+    version = "0.14.2";
+    sha256 = "0iiaa6vnavb68s7zr3f773qwykfw2s3a81a2cyq6xja03si6a83w";
+  }
+  {
+    name = "crypto";
+    version = "2.1.5";
+    sha256 = "1s77x3n71kajbszqgiahr1czllmgll6b5nr4qpk4n557qvrjiriw";
+  }
+  {
+    name = "csslib";
+    version = "0.16.2";
+    sha256 = "0acjg16xp68x1m7dp1gfy7q2xzkfzlqx5allf1j5amnrc41hry65";
+  }
+  {
+    name = "dds";
+    version = "1.6.0";
+    sha256 = "1pbpy7w129h8lspdn8m86pmhiy3m6lrmj0xdxx7agj8dgmj0d990";
+  }
+  {
+    name = "devtools";
+    version = "0.9.5";
+    sha256 = "0x973065mm0lpc4lha6iqjn4jn7b8iqia9fvns5mwahb1pqpxsrd";
+  }
+  {
+    name = "devtools_server";
+    version = "0.9.5";
+    sha256 = "17ggswvw11ai9xp33f3llw2ab02lrhkjn62ziivs5ggahjczp53z";
+  }
+  {
+    name = "devtools_shared";
+    version = "0.9.5";
+    sha256 = "15k24cxkjjs87c2x61dhmd20gpi8jamwj8ix6g33np98kkf04a8g";
+  }
+  {
+    name = "dwds";
+    version = "7.1.1";
+    sha256 = "0sk5ix0pb18857yi1gq2ldyfgs5ljwz0g5pdp8ijv16rijlf6v7c";
+  }
+  {
+    name = "fake_async";
+    version = "1.2.0-nullsafety.3";
+    sha256 = "0fpqm42cg81zsbylvwj7cxnfa3da15xyd80hn001c1lzpp36wz9r";
+  }
+  {
+    name = "file";
+    version = "6.0.0-nullsafety.4";
+    sha256 = "17968bba3jnizmqfadx08p8m6xb3sxqm4kwjrz3r8cxz2qism09d";
+  }
+  {
+    name = "file_testing";
+    version = "2.1.0";
+    sha256 = "1yf9868n80iav2lddir1dh769pgbki9ipgvvm6lsbr6q53fslyfl";
+  }
+  {
+    name = "fixnum";
+    version = "0.10.11";
+    sha256 = "0li0dhpykrcicfk9zf36a0ci63wvi65kh6gcvmgqz2l9n6n8sa9v";
+  }
+  {
+    name = "flutter_template_images";
+    version = "1.0.1";
+    sha256 = "1hvphgkz3m79pjlhp06lq50jycvi3yjkcm3fkbqq3hsv4fsmzymb";
+  }
+  {
+    name = "glob";
+    version = "1.2.0";
+    sha256 = "0fxlrsxrizf8abfgb9xcvcbd9pc864w2x8x77k17wkhz2an9lgbs";
+  }
+  {
+    name = "html";
+    version = "0.14.0+4";
+    sha256 = "1063362flidzkk0ql5v3ljpb9m5zfy9xf6k45rwapv45dw5g1w29";
+  }
+  {
+    name = "http";
+    version = "0.12.2";
+    sha256 = "0avsm7pjhsfi9jdw32cjp8vr0xz4lqd0m1n4madjpx9licwn44j1";
+  }
+  {
+    name = "http_multi_server";
+    version = "2.2.0";
+    sha256 = "1rg5aijr9wd7qb911jgpd97m9xh36mv56h4z82y3gp3mnxcfznlh";
+  }
+  {
+    name = "http_parser";
+    version = "3.1.4";
+    sha256 = "1aaxp6q2zq55rknw7r5qq3b68q15izd610w6wscgq0p4x4k0cawx";
+  }
+  {
+    name = "intl";
+    version = "0.17.0-nullsafety.2";
+    sha256 = "14fchfqx54jyfaws8mkj882zli8frmzyhi7wa5c749w1bs5j1zsc";
+  }
+  {
+    name = "io";
+    version = "0.3.4";
+    sha256 = "010sv120dj2cvxdxmzfmc6azawd8dqa9l6g5q97h9c8qxsc5b5b9";
+  }
+  {
+    name = "js";
+    version = "0.6.3-nullsafety.3";
+    sha256 = "153vwfv6zhgyf05l8kgv2hmndwzdvlhdp1xmx7wqbmb7gm0mkdfz";
+  }
+  {
+    name = "json_annotation";
+    version = "3.1.1";
+    sha256 = "0698klgskpavqm37iyn19sd0fq67sqf16c3vrg43hqsxn8279v3c";
+  }
+  {
+    name = "json_rpc_2";
+    version = "2.2.2";
+    sha256 = "17rwgklcwcq799gcwwk5q63nxz4ravsjnvvli7nwr4r3n7z5phrp";
+  }
+  {
+    name = "logging";
+    version = "0.11.4";
+    sha256 = "1mrssf4j5r30skh5ygqvngqchpcqzfba9wssfk7r18z277105513";
+  }
+  {
+    name = "matcher";
+    version = "0.12.10-nullsafety.3";
+    sha256 = "1da10q4mrb8wwg4larvqiwcigjnc3ljyfa4rbrv8g3b4l421dr77";
+  }
+  {
+    name = "meta";
+    version = "1.3.0-nullsafety.6";
+    sha256 = "0sr31zd13y34sxsxlkrxrgr86sn0xk8w3qkbzaz4cplf2mcd135f";
+  }
+  {
+    name = "mime";
+    version = "0.9.7";
+    sha256 = "1r23yzfhz5l3b475j3w3fl7jgvm4hlvl1h77vrqbs3z3yh9z6hah";
+  }
+  {
+    name = "mockito";
+    version = "4.1.1";
+    sha256 = "1gjwbilmiwcsg4yffvdmisa55xv735731rphah05wc4vgbhic5jc";
+  }
+  {
+    name = "multicast_dns";
+    version = "0.2.2";
+    sha256 = "17c5n7x7q0af982y67vkxv5k1g32y3f42nhxskr0rc2dcwaq4r82";
+  }
+  {
+    name = "mustache_template";
+    version = "1.0.0+1";
+    sha256 = "048mi3s6gw913v4ma3zymkivvv4d421mh9aiy2732bzfar0m4dkc";
+  }
+  {
+    name = "native_stack_traces";
+    version = "0.3.7";
+    sha256 = "12j3g2hic6bznzbj6pl457r4awyz3c4b0wdk349mvganpjs74hq9";
+  }
+  {
+    name = "node_interop";
+    version = "1.2.1";
+    sha256 = "1hdqqhh5n36sl6yhg6s81faq1hwbch7cfzlbgccwsc4nb3w7a3ci";
+  }
+  {
+    name = "node_io";
+    version = "1.1.1";
+    sha256 = "0wvvy5753ljdcrbq0kvci0mdj6x5m2h7g5c6z15c8fjl095iv06x";
+  }
+  {
+    name = "node_preamble";
+    version = "1.4.12";
+    sha256 = "14yqi827jrv60lwzzq03a6ck8l821l9127jpfiwcm9ic5c4cvyqr";
+  }
+  {
+    name = "package_config";
+    version = "1.9.3";
+    sha256 = "1j8jvcyjp5nd8z7cn06rhadbc6s4d3m68s3p4spnlwy9z2ssrcq8";
+  }
+  {
+    name = "path";
+    version = "1.8.0-nullsafety.3";
+    sha256 = "06d1z0r01kz0x6an7d63779jhqilil43s5ylvwscjrhgql7qfi2m";
+  }
+  {
+    name = "pedantic";
+    version = "1.10.0-nullsafety.3";
+    sha256 = "0zmywwvh92hwpygcmb81mw46khfn51xscmp1bk5fzm4grla87sjm";
+  }
+  {
+    name = "petitparser";
+    version = "3.1.0";
+    sha256 = "1wpmkas7q0xqma3flpfah1kj0h6c4174kxfih74g0gzhbcsgj1zy";
+  }
+  {
+    name = "platform";
+    version = "3.0.0-nullsafety.4";
+    sha256 = "05lidm6qz097kxgw5843ip86pjnhhhlr76vw2m79v09dxhhhnx74";
+  }
+  {
+    name = "pool";
+    version = "1.5.0-nullsafety.3";
+    sha256 = "1zhx8jlsqwsgh6k23r8x8m4b6yrazwmx84wi4fb8bhf82r4bbj01";
+  }
+  {
+    name = "process";
+    version = "4.0.0-nullsafety.4";
+    sha256 = "1y1fgr9b61m31hbinmxcjk5cf0zjh094icwdckipnik48fkr2ivp";
+  }
+  {
+    name = "pub_semver";
+    version = "1.4.4";
+    sha256 = "0715yyj0da92chbf2r3cb3d56g0y1v31cj42zsn5rf6cmq5fql9w";
+  }
+  {
+    name = "pubspec_parse";
+    version = "0.1.5";
+    sha256 = "015ykf4fq9kk0mlj1ja8li7ilnf1r7rh6kbkn52jxzzwvvqwjx72";
+  }
+  {
+    name = "quiver";
+    version = "2.1.5";
+    sha256 = "096asdn2ky8b96a5d36n64blv05s7dwsivjl1d0a2p9pwv8plgz8";
+  }
+  {
+    name = "shelf";
+    version = "0.7.5";
+    sha256 = "1zw64hj2a49p0w0yglfy1ll78zqpqiiqq3yd3kf7b017h5ss0mn2";
+  }
+  {
+    name = "shelf_packages_handler";
+    version = "2.0.0";
+    sha256 = "0a2p9vlpm3fshpzdkrw8vgm64r8y4rk61zz8af07na31lq25mzna";
+  }
+  {
+    name = "shelf_proxy";
+    version = "0.1.0+7";
+    sha256 = "1hcs4n8a6jaczwnzpcgfh4k1a8c6baj8qjw52qyw8fy78zy8fhl1";
+  }
+  {
+    name = "shelf_static";
+    version = "0.2.8";
+    sha256 = "0902d7mfk59dx184qhcn7nhaw4fxrdayrcy4hns9hvi8h5yaa810";
+  }
+  {
+    name = "shelf_web_socket";
+    version = "0.2.3";
+    sha256 = "1ivfi1xh5jl0gr6qn0jih713jwpg2xmxkkpx72lkkg38ip5q5zri";
+  }
+  {
+    name = "source_map_stack_trace";
+    version = "2.1.0-nullsafety.4";
+    sha256 = "07118pyicbcagxz2ww5m63fhdfc1sff7byj4xmqnn1j1jvkw05n1";
+  }
+  {
+    name = "source_maps";
+    version = "0.10.10-nullsafety.3";
+    sha256 = "0p7md02m28dfapkmfvbvmc9c28l00wm1y4n51jbpd75wkmvfbn8r";
+  }
+  {
+    name = "source_span";
+    version = "1.8.0-nullsafety.4";
+    sha256 = "0f05izncanjsj832r2f417f1lknghamf40xk27aadn7x212jhpcq";
+  }
+  {
+    name = "sse";
+    version = "3.6.0";
+    sha256 = "18algk8cmgs9px9zmc00krmsh4vcy9zav0h27ki0splhrjdfaq52";
+  }
+  {
+    name = "stack_trace";
+    version = "1.10.0-nullsafety.6";
+    sha256 = "0qjfg85f1113fs1agf7idifji3g44s01w64yasvzi9bmjgksz650";
+  }
+  {
+    name = "stream_channel";
+    version = "2.1.0-nullsafety.3";
+    sha256 = "0flya8yk46ijp8hd1yzz93fpj86h0zd24fk0m8x8cb67l587amnb";
+  }
+  {
+    name = "string_scanner";
+    version = "1.1.0-nullsafety.3";
+    sha256 = "189f5biw9iajdpwvfnwfh5738hx1pr1z8wisnlm555qy82g2lvf3";
+  }
+  {
+    name = "sync_http";
+    version = "0.2.0";
+    sha256 = "03qfpv6xg854384zniw4k01n61fcb9zk1g5v2ra7gyxmng3cdrcy";
+  }
+  {
+    name = "term_glyph";
+    version = "1.2.0-nullsafety.3";
+    sha256 = "0383sx9b9w8rz4mwva62wnp0nffv8a8pqdvgkmlz7m5h3vlgpz94";
+  }
+  {
+    name = "test";
+    version = "1.16.0-nullsafety.9";
+    sha256 = "1ix8pm5pxx05gkc82800lwyjwsz9q4l62f3yzprk7xzx8ksvxmgz";
+  }
+  {
+    name = "test_api";
+    version = "0.2.19-nullsafety.6";
+    sha256 = "1nnp11l0504x6ymgbdij9g0hgrhcdy9gnq8bjbcnfwvj6p4vy05f";
+  }
+  {
+    name = "test_core";
+    version = "0.3.12-nullsafety.9";
+    sha256 = "0dsxvpn1hnw5r1hy40bgmz4cjp0256k65ixshfwbcdxs6rb4wqbj";
+  }
+  {
+    name = "typed_data";
+    version = "1.3.0-nullsafety.5";
+    sha256 = "07ag7yfcndmrykn42pgnia80cys15b7rf599145qk12s5zbhvhaf";
+  }
+  {
+    name = "usage";
+    version = "3.4.2";
+    sha256 = "0l7dj8d4203qcr2pzjfchdqr31rhacvdaqhkkn643cxjh9iclw5m";
+  }
+  {
+    name = "uuid";
+    version = "2.2.2";
+    sha256 = "1c41dm6aw2q0a9qjyjvgm5a367lxy9g77ycrqv548yw0dvcab5mx";
+  }
+  {
+    name = "vm_service";
+    version = "5.5.0";
+    sha256 = "1apqs0wp5d239pzx4b64v9gz5l79xiwnh1jknjypw0zqbgw6327c";
+  }
+  {
+    name = "vm_snapshot_analysis";
+    version = "0.5.5";
+    sha256 = "17bh8i8ra96rkwmw7rrsvg36gb5qaf3awymhkvflrm5ppbij5hf5";
+  }
+  {
+    name = "watcher";
+    version = "0.9.7+15";
+    sha256 = "1rw0m9iz20475i19g76v04q0v7v5qjb6sa096c2wb8qssi92dcy0";
+  }
+  {
+    name = "web_socket_channel";
+    version = "1.1.0";
+    sha256 = "1pjqqa73iaqhdzvvwxbl6290biirznc3hp59dmzrpbgisg8wc4kp";
+  }
+  {
+    name = "webdriver";
+    version = "2.1.2";
+    sha256 = "14kw5nvgj32vr6r5k4vlgaw51b4z08npyxb6s5jsp5ppa7lw0ymg";
+  }
+  {
+    name = "webkit_inspection_protocol";
+    version = "0.7.4";
+    sha256 = "0ivpadbbwixj5pv6zkfn3873bz4g6xjvb89scx2g2k9i3c0aknrj";
+  }
+  {
+    name = "xml";
+    version = "4.5.1";
+    sha256 = "0lh7ah3k3pi8lnqq8h6ysd86sgf0r9yd7prwfjl6zdppvk3n4hjc";
+  }
+  {
+    name = "yaml";
+    version = "2.2.1";
+    sha256 = "0x4j9dl0s837i534a5h2hwnwnfp2122w0qac9rk3y4asxzzxghsy";
+  }
+]

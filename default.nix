fe05f8d6eb3e2b9045766fe10f0b26340b2f4b6b
Joshua Tacoma <jtacoma@google.com>
2020-12-29 22:18:07 -0500

    nix: make it easier to use nix-build to build random things
    This lets me run `nix-build -A pkgs.flutter` in the root of this repo, for example.

--- default.nix
+++ default.nix
@@ -1,6 +1,4 @@
 { project ? import ./nix { }, pkgs ? project.pkgs }:
 {
-  hello = import ./hello.nix { inherit (pkgs) stdenv fetchurl perl; };
-  helloworld = pkgs.writeShellScriptBin "hellome" "echo Hello $USER";
-  fdroid = import ./fdroid.nix { inherit project pkgs; };
+  inherit pkgs;
 }

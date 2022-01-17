f96e9bf4688110898229ad6b26295b6a4286517b
Joshua Tacoma <joshua.tacoma@gmail.com>
2022-01-17 11:49:07 -0500

    LICENSE: add copyright headers and "generated" comments

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,3 +1,5 @@
+# TODO: try removing this file and see if everything still works.
+
 # Example shell.nix. Copy this to the parent directory to use nix-shell to load
 # a consistent development environment for this project.
 
